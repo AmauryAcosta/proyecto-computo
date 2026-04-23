14f40815dd2ef577a36d471f8307106793dcdaa9
Amaury Acosta <amaury.4everaco@outlook.com>
2026-04-22 18:27:42 -0600

    feat: setup inicial Vite + React + Router + Tailwind + axios

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,11 @@
+// src/main.jsx — este casi no se toca, solo verifica que esté así
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
